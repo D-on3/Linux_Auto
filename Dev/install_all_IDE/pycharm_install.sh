e96ae1983626a9abfdd9551e90fdb46d1ec6972b
d-on3 <dnedyalkov396@gmail.com>
2022-07-03 00:00:30 +0300

    init

---
+++ Dev/install_all_IDE/pycharm_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+mkdir /home/aod/IDE
+mkdir /home/aod/IDE/Pycharm
+cd /home/aod/IDE/Pycharm
+wget https://download-cdn.jetbrains.com/python/pycharm-community-2022.1.3.tar.gz
+tar xvf pycharm-community-2022.1.3.tar.gz
+cd pycharm-community-2022.1.3
+cd bin
+
+
+
